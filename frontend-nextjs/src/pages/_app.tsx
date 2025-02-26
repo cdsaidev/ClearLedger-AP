3151ed2297f65cb7cd6eed6a8df7eb26e47e6ab9
chris9753 <mr.chrisdasilver@gmail.com>
2025-02-26 08:38:55 -0300

    Wrap application in QueryClientProvider to enable React Query functionality

--- frontend-nextjs/src/pages/_app.tsx
+++ frontend-nextjs/src/pages/_app.tsx
@@ -1,11 +1,16 @@
-import '../styles/globals.css';
-import Layout from '../components/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
